9d0f87432b6c143b1366c5dd2ace3d22a48f5fbb
Nathan Glover <nathan@glovers.id.au>
2019-01-24 19:19:17 +0800

    Fixed bash script

--- cloudformation_deploy.sh
+++ cloudformation_deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+BUCKET_NAME=devopstar
+
 ## Creates S3 bucket
-# aws s3 mb s3://devopstar
+aws s3 mb s3://$BUCKET_NAME
 
 ## S3 cloudformation deployments
 ### Base
-aws s3 cp cloudformation/base/fargate-cluster.yaml s3://devopstar/resources/service-slayer/cloudformation/base/fargate-cluster.yaml
-aws s3 cp cloudformation/base/fargate-service.yaml s3://devopstar/resources/service-slayer/cloudformation/base/fargate-service.yaml
-aws s3 cp cloudformation/base/vpc-networking.yaml s3://devopstar/resources/service-slayer/cloudformation/base/vpc-networking.yaml
+aws s3 cp cloudformation/base/fargate-cluster.yaml s3://$BUCKET_NAME/resources/service-slayer/cloudformation/base/fargate-cluster.yaml
+aws s3 cp cloudformation/base/fargate-service.yaml s3://$BUCKET_NAME/resources/service-slayer/cloudformation/base/fargate-service.yaml
+aws s3 cp cloudformation/base/vpc-networking.yaml s3://$BUCKET_NAME/resources/service-slayer/cloudformation/base/vpc-networking.yaml
 ### Extended
-aws s3 cp cloudformation/extended/fargate-service-arbiter.yaml s3://devopstar/resources/service-slayer/cloudformation/extended/fargate-service-arbiter.yaml
+aws s3 cp cloudformation/extended/fargate-service-arbiter.yaml s3://$BUCKET_NAME/resources/service-slayer/cloudformation/extended/fargate-service-arbiter.yaml
 ### CI/CD
-aws s3 cp cloudformation/cicd/codebuild.yaml s3://devopstar/resources/service-slayer/cloudformation/cicd/codebuild.yaml
+aws s3 cp cloudformation/cicd/codebuild.yaml s3://$BUCKET_NAME/resources/service-slayer/cloudformation/cicd/codebuild.yaml
